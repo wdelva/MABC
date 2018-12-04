cff4a4a4299fa056c0f0fe0d88e831ae1e27c64c
wdelva <Wim.Delva@UGent.be>
2018-12-04 17:05:02 +0200

    MPI parallel functionality added

---
+++ R/model.mpi.run.R
@@ -0,0 +1,38 @@
+#' Distribute model runs over multiple nodes
+#'
+#' Allows faster execution of each wave of simulations
+#'
+#' @param model Wrapper function for the model
+#' @param actual.input.matrix Matrix with parameter combinations to be run
+#' @param seed_count Origin of random number seed
+#' @param slaves Number of slave workers available for parallel running of the model
+#' @return a matrix of model features and the seed of the random number
+#'   generator
+#' @import doMPI
+#' @import foreach
+#' @import Rmpi
+#' @export
+
+model.mpi.run <- function(model,
+                          actual.input.matrix,
+                          seed_count = 0,
+                          slaves = 3){
+  mpi.comm.set.errhandler(comm = 1)
+  cl <- startMPIcluster(count = slaves)
+  registerDoMPI(cl)
+
+  nb_simul <- nrow(actual.input.matrix)
+
+  modelfeatures <- foreach(irun = 1:nb_simul,
+                           .inorder=TRUE,
+                           .combine="rbind") %dopar% {
+                             seed <- seed_count + irun
+                             model(c(seed, actual.input.matrix[irun, ]))
+                           }
+  closeCluster(cl)
+
+  modelfeatures.array <- as.array(cbind(modelfeatures,
+                                        seed_count + 1:nb_simul))
+  dimnames(modelfeatures.array) <- NULL
+  return(modelfeatures.array)
+}
