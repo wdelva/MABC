3923a1aa200a9257d884c8dc9e577a47f01cbbad
wdelva <Wim.Delva@UGent.be>
2018-12-05 22:35:00 +0200

    snow version of multi-node parallel

---
+++ R/model.snow.run.R
@@ -0,0 +1,34 @@
+#' Distribute model runs over multiple nodes
+#'
+#' Allows faster execution of each wave of simulations
+#'
+#' @param model Wrapper function for the model
+#' @param actual.input.matrix Matrix with parameter combinations to be run
+#' @param seed_count Origin of random number seed
+#' @param n_cores Number of slave workers available for parallel running of the model
+#' @return a matrix of model features and the seed of the random number
+#'   generator
+#' @import foreach
+#' @export
+
+model.snow.run <- function(model,
+                          actual.input.matrix,
+                          seed_count = 0,
+                          n_cores = 3){
+  cl <- snow::makeCluster(n_cores, type = "SOCK")
+  doSNOW::registerDoSNOW(cl)
+
+  nb_simul <- nrow(actual.input.matrix)
+
+  modelfeatures <- foreach(irun = 1:nb_simul,
+                           .inorder=TRUE,
+                           .combine="rbind") %dopar% {
+                             seed <- seed_count + irun
+                             model(c(seed, actual.input.matrix[irun, ]))
+                           }
+  snow::stopCluster(cl)
+  modelfeatures.array <- as.array(cbind(modelfeatures,
+                                        seed_count + 1:nb_simul))
+  dimnames(modelfeatures.array) <- NULL
+  return(modelfeatures.array)
+}
